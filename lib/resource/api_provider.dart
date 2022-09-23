5afeb863ff0bfbc6ed7f151c36f6860cae571035
diponkar <44252172+Diponkar-Saha@users.noreply.github.com>
2022-09-23 12:50:51 +0600

    apiprovider

---
+++ lib/resource/api_provider.dart
@@ -0,0 +1,28 @@
+import 'package:dio/dio.dart';
+import 'package:task_bloc/utils/util_constants.dart';
+
+import '../model/product_response.dart';
+import '../model/search_response.dart';
+class ApiProvider {
+  final Dio _dio = Dio();
+
+  Future<SearchResponse> fetchCovidList(String slug) async {
+    try {
+      Response response = await _dio.get('$BASE_URL/product/search-suggestions/?limit=10&search=$slug');
+      return SearchResponse.fromJson(response.data);
+    } catch (error, stacktrace) {
+      print("Exception occured: $error stackTrace: $stacktrace");
+      return SearchResponse.withError("Data not found / Connection issue");
+    }
+  }
+  Future<ProductResponse> fetchProduct(String slug) async {
+    try {
+
+      Response response = await _dio.get('$BASE_URL/product-details/$slug/');
+      return ProductResponse.fromJson(response.data);
+    } catch (error, stacktrace) {
+      print("Exception occured: $error stackTrace: $stacktrace");
+      return ProductResponse.withError("Data not found / Connection issue");
+    }
+  }
+}
